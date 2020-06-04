52c665b866a48d0c15c653d75a8fa9de7f2b7cf7
habeelwali <habeelwal3@gmail.com>
2020-06-04 14:40:04 +0000

    added

---
+++ bootcamp-react-app/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div>
+      HELLO PIAIC TEAM
+    </div>
+  );
+}
+
+export default App;
